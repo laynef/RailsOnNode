9971b52068ac60f330c93789a0fcd5452b332951
Layne Faler <laynefaler@gmail.com>
2018-08-22 08:41:35 -0700

    Updates to page for react and it's replacements

--- templates/assets/page.jsx
+++ templates/assets/page.jsx
@@ -1,65 +1,45 @@
 import 'intersection-observer';
 import 'babel-polyfill';
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import createStore from '../redux/store';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import { renderRoutes } from 'react-router-config';
-import { declareMainRoutes } from './routes';
 
-const route = /* CLI: Route Path */;
-
-require(route);
-import 'velocity-animate';
-import 'velocity-animate/velocity.ui';
+import '/* CLI: Route Path */';
 
 function createStore(data) {
 
+};
+
+class Application extends Component {
+    render() {
+        return (
+            <div>Hello World</div>
+        );
+    };
 }
 
-const dest = document.getElementById('main-app');
+const dest = document.getElementById('app');
 const store = createStore(window.STORE);
 
-const component = (
-	<BrowserRouter>
-		{renderRoutes(declareMainRoutes(true))}
-	</BrowserRouter>
-);
-
 const application = (
-	<Provider store={store}>
-		{component}
-	</Provider>
+    <Provider store={store}>
+        {Application}
+    </Provider>
 );
 
 ReactDOM.render(application, dest);
 
-
-if ((process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'staging')) {
-	window.React = React; // enable debugger
-}
-
 /* eslint-disable */
-if ((process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'staging') && module.hot) {
-	module.hot.accept([
-		'./routes',
-		'../redux/store',
-	], () => {
-		const newStore = require('../redux/store')(window.STORE);
-		const newRoutes = require('./routes').declareMainRoutes;
-
-		const newComponent = (
-			<BrowserRouter>
-				{renderRoutes(newRoutes(true))}
-			</BrowserRouter>
-		);
+if (process.env.NODE_ENV !== 'production' && module.hot) {
+	module.hot.accept([], () => {
+		const newStore = createStore(window.STORE);
 
 		const newApplication = (
 			<Provider store={newStore}>
-				{newComponent}
+				{Application}
 			</Provider>
-		);
+        );
+        
 		ReactDOM.render(newApplication, dest);
 	});
 }
