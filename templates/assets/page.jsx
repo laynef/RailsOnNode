1a699c8dc0043c65edcfe30d961c79914976bf3c
Layne Faler <laynefaler@gmail.com>
2018-08-22 08:12:01 -0700

    Updates to jsx for react

---
+++ templates/assets/page.jsx
@@ -0,0 +1,63 @@
+import 'intersection-observer';
+import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import createStore from '../redux/store';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { renderRoutes } from 'react-router-config';
+import { declareMainRoutes } from './routes';
+
+import './route-path';
+import 'velocity-animate';
+import 'velocity-animate/velocity.ui';
+
+function createStore(data) {
+
+}
+
+const dest = document.getElementById('main-app');
+const store = createStore(window.STORE);
+
+const component = (
+	<BrowserRouter>
+		{renderRoutes(declareMainRoutes(true))}
+	</BrowserRouter>
+);
+
+const application = (
+	<Provider store={store}>
+		{component}
+	</Provider>
+);
+
+ReactDOM.render(application, dest);
+
+
+if ((process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'staging')) {
+	window.React = React; // enable debugger
+}
+
+/* eslint-disable */
+if ((process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'staging') && module.hot) {
+	module.hot.accept([
+		'./routes',
+		'../redux/store',
+	], () => {
+		const newStore = require('../redux/store')(window.STORE);
+		const newRoutes = require('./routes').declareMainRoutes;
+
+		const newComponent = (
+			<BrowserRouter>
+				{renderRoutes(newRoutes(true))}
+			</BrowserRouter>
+		);
+
+		const newApplication = (
+			<Provider store={newStore}>
+				{newComponent}
+			</Provider>
+		);
+		ReactDOM.render(newApplication, dest);
+	});
+}
