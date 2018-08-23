0f90c7491ba68e1c4cd713d13eecdf04f81e0dcc
Layne Faler <laynefaler@gmail.com>
2018-08-22 18:03:18 -0700

    Updates to settings for redux

--- actions/sources/commands/settings.js
+++ actions/sources/commands/settings.js
@@ -83,7 +83,7 @@ const command = (type, options) => {
 
             const regexReduxString = reduxRecursive.join('/');
 
-            shell.cp(path.join(__dirname, '..', '..', '..', 'templates', 'redux', before), path.join(root, 'assets', after, 'redux'))
+            shell.cp(path.join(__dirname, '..', '..', '..', 'templates', 'redux', before), path.join(root, 'assets', CURR_JS, 'redux'))
 
             fs.writeFileSync(pathNames, str.replace(regexRedux, regexReduxString).replace(regexStyles, `import "${regexStylesString}";`));
         },
