8e86b78ecb8a7757c8c0fed4d9349a8bc3b8bf2d
Layne Faler <laynefaler@gmail.com>
2018-08-26 14:57:23 -0700

    Temporary removal for commands in development

--- actions/sources/index.js
+++ actions/sources/index.js
@@ -1,10 +1,18 @@
 const fs = require('fs');
 const path = require('path');
 
+const tempRemoval = {
+    'settings': 'settings',
+    'new-redux-reducer': 'new-redux-reducer',
+    'setup-data-base': 'setup-data-base',
+};
+
 const index = fs.readdirSync(path.join(__dirname, 'commands')).map(e => e.replace(RegExp('.js', 'ig'), '')).reduce((accumulation, file) => {
-    const shortcut = file.split('-').map(e => e[0]).join('').toLowerCase();
-    accumulation[file] = require(path.join(__dirname, 'commands', file));
-    accumulation[shortcut] = require(path.join(__dirname, 'commands', file));
+    if (!tempRemoval[file]) {
+        const shortcut = file.split('-').map(e => e[0]).join('').toLowerCase();
+        accumulation[file] = require(path.join(__dirname, 'commands', file));
+        accumulation[shortcut] = require(path.join(__dirname, 'commands', file));
+    }
     return accumulation;
 }, {});
 
