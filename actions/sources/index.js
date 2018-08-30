5c80976f83b3600b394825dee731b85d77b0dfdf
Layne Faler <laynefaler@gmail.com>
2018-08-30 15:07:45 -0700

    Tests are fully operational

--- actions/sources/index.js
+++ actions/sources/index.js
@@ -1,16 +1,10 @@
 const fs = require('fs');
 const path = require('path');
 
-const tempRemoval = {
-    'text-editor-configs': 'text-editor-configs',
-};
-
 const index = fs.readdirSync(path.join(__dirname, 'commands')).map(e => e.replace(RegExp('.js', 'ig'), '')).reduce((accumulation, file) => {
-    if (!tempRemoval[file]) {
-        const shortcut = file.split('-').map(e => e[0]).join('').toLowerCase();
-        accumulation[file] = require(path.join(__dirname, 'commands', file));
-        accumulation[shortcut] = require(path.join(__dirname, 'commands', file));
-    }
+    const shortcut = file.split('-').map(e => e[0]).join('').toLowerCase();
+    accumulation[file] = require(path.join(__dirname, 'commands', file));
+    accumulation[shortcut] = require(path.join(__dirname, 'commands', file));
     return accumulation;
 }, {});
 
