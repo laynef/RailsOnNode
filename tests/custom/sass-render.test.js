069c417d0182d413052f01508e18e61acc1fb15b
Layne Faler <laynefaler@gmail.com>
2018-09-02 16:03:00 -0700

    Updates to tests on neutron

---
+++ tests/custom/sass-render.test.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const shell = require('shelljs');
+
+module.exports = () => {
+
+    const root = path.join(__dirname, '..', '..');
+
+    shell.exec(`node-rails create temp`);
+    shell.cd(path.join(root, 'temp'));
+    shell.exec(`npm install`);
+    shell.exec(`npm run build:dev`);
+    shell.exec(`npm test`);
+
+};
