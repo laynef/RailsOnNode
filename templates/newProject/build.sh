b374343043f7937c868945a6478006f4f1d81564
Layne Faler <laynefaler@gmail.com>
2018-10-12 13:55:26 -0700

    Updates to deployment integration

--- templates/newProject/build.sh
+++ templates/newProject/build.sh
@@ -1,13 +1,14 @@
 CURR_DIR=$(PWD)
+DATE=`date '+%Y-%m-%d-%H:%M:%S'`
+
+
+rm -rf node_modules deploy-src build
+cd ..
+cp -R $CURR_DIR ./deploy-src
+mv ./deploy-src $CURR_DIR/deploy-src
+cd $CURR_DIR
 npm install
 npm run build:prod
-rm -rf node_modules deploy-src build
-
-cd ..
-cp -R "${CURR_DIR}" "./deploy-src"
-mv "./deploy-src" "${CURR_DIR}/deploy-src"
-cd "${CURR_DIR}"
-npm install
 npm run babel:build
 rm -rf ./deploy-src
 cp -R ./package.json ./build/package.json
@@ -19,3 +20,9 @@ cp -R ./assets/img ./build/assets/img
 rm -rf ./build/assets/dist
 cp -R ./assets/dist ./build/assets/dist
 cp -R ./assets/manifest.json ./build/assets/manifest.json
+cd $CURR_DIR/build
+rm -rf *.zip
+zip -r build-$DATE.zip .
+mv build-$DATE.zip ..
+cd $CURR_DIR
+rm -rf ./build
