ba2349d5b8de6ff0baa99fe68faff31894d6b355
Layne Faler <laynefaler@gmail.com>
2019-08-04 14:57:20 -0700

    Deleted testing server

--- tests/tutorial/postcss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ file, options, env }) => ({
-    parser: file.extname === '.sss' ? 'sugarss' : false, // Handles `.css` && '.sss' files dynamically
-    plugins: {
-        'postcss-preset-env': {},
-        'autoprefixer': {},
-        'postcss-import': {},
-        'cssnano': env === 'production' ? {} : false,
-    },
-});
