df83ba980776d9ab76a1e94b30030bb805921538
Layne Faler <laynefaler@gmail.com>
2019-08-24 16:32:31 -0700

    Updates to actions for create to new

---
+++ actions/sources/commands/console.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const shell = require('shelljs');
+const { root_directory } = require('../utils');
+
+const description = 'Access all of your Sequelize models with Node console';
+
+const documentation = () => {
+    console.cyan(`
+Command:
+
+node-rails console
+    `);
+};
+
+const command = (...args) => {
+    if (!root_directory()) {
+        console.red('Must run this command in the root directory of your project.');
+        return;
+    }
+
+    const root = process.cwd();
+    const strs = [...args];
+    strs.pop();
+    const str = strs.join(' ');
+    const bin = path.join(root, 'bin', 'console');
+    shell.exec(`${bin} ${str}`);
+};
+
+module.exports = {
+    command,
+    description,
+    documentation,
+};
